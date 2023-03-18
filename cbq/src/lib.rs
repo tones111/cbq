04be4c4a331244d90dadda6dd8c4767cedb4e4ca
Paul Sbarra <Sbarra.Paul@gmail.com>
2023-03-17 20:50:50 -0500

    simple push/pop

--- cbq/src/lib.rs
+++ cbq/src/lib.rs
@@ -1,11 +1,45 @@
+#![allow(clippy::missing_safety_doc)]
+
+use crossbeam::queue::ArrayQueue;
+
+pub struct Cbq {
+    q: ArrayQueue<u8>,
+}
+
+//impl Drop for Cbq {
+//    fn drop(&mut self) {
+//        println!("dropping Cbq");
+//    }
+//}
+
 #[no_mangle]
-pub extern "C" fn cbq__construct() -> () {
-    println!("todo: construct");
+pub extern "C" fn cbq__construct(cap: u32) -> *mut Cbq {
+    Box::into_raw(Box::new(Cbq {
+        q: ArrayQueue::new(usize::try_from(cap).unwrap()),
+    }))
 }
 
 #[no_mangle]
-pub extern "C" fn cbq__destruct() -> () {
-    println!("todo: destruct");
+pub unsafe extern "C" fn cbq__destruct(ptr: *mut Cbq) {
+    drop(Box::from_raw(ptr))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn cbq__push(ptr: *mut Cbq, val: u8) -> u8 {
+    let cbq = &*ptr;
+    cbq.q.push(val).is_ok().into()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn cbq__pop(ptr: *mut Cbq, val: *mut u8) -> u8 {
+    let cbq = &*ptr;
+    match cbq.q.pop() {
+        Some(v) => {
+            *val = v;
+            1
+        }
+        None => 0,
+    }
 }
 
 //#[cfg(test)]
--- main.cpp
+++ main.cpp
@@ -1,16 +1,31 @@
+#include <cassert>
+#include <cstdint>
 #include <iostream>
 
 extern "C"
 {
-   void* cbq__construct();
-   void* cbq__destruct();
+   void* cbq__construct(uint32_t cap);
+   void cbq__destruct(void* cbq);
+
+   uint8_t cbq__push(void* cbq, uint8_t val);
+   uint8_t cbq__pop(void* cbq, uint8_t *val);
 }
 
 int main(int argc, char *argv[])
 {
    std::cout << "hello world" << std::endl;
-   cbq__construct();
-   cbq__destruct();
+   void *q = cbq__construct(2);
+   assert(cbq__push(q, 1));
+   assert(cbq__push(q, 2));
+   assert(!cbq__push(q, 3)); // full
+
+   uint8_t v;
+   assert(cbq__pop(q, &v));
+   assert(v == 1);
+   assert(cbq__pop(q, &v));
+   assert(v == 2);
+   assert(!cbq__pop(q, &v)); // empty
+   cbq__destruct(q);
 
    return 0;
 }
